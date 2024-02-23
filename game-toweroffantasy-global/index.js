94c233b99f1d62133f0b6434da2b06d9863f457e
soevielofficial <soevielofficial@gmail.com>
2024-02-24 02:41:19 +0700

    updated `registerInstaller`
    Updated mod installation patters, in order to prevent conflicts between chinese, global, korean clients.

--- game-toweroffantasy-global/index.js
+++ game-toweroffantasy-global/index.js
@@ -62,7 +62,7 @@ function main(context) {
       return fs.ensureDirAsync(path.join(discovery.path, 'Hotta', 'Content', 'Paks', '~mods'));
     }
 
-    context.registerInstaller('toweroffantasy', 25, testSupportedContent, installContent);
+    context.registerInstaller('toweroffantasy-global', 25, testSupportedContent, installContent);
 
     const MOD_FILE_EXT = ".pak";
 
